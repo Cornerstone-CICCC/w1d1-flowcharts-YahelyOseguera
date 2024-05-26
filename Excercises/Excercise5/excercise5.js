db98a72d1187f1d8326d6321483ab03d2e67703b
Yahely <yahelyoseguera@hotmail.es>
2024-05-26 14:58:48 -0700

    Excercise5

---
+++ Excercises/Excercise5/excercise5.js
@@ -0,0 +1,14 @@
+
+let grade = [80, 60 , 40];
+let StudentGrade = 95;
+
+if (StudentGrade >= grade[0]){
+    console.log("Student grade is A");
+
+}else if (StudentGrade>= grade[1]){
+    console.log("Student grade is B");
+}else if (StudentGrade>= grade[2]){
+    console.log("Student grade is C");
+}else {
+    console.log("Student grade is F");
+}
